37cf38783446c1cd60669213311bc3be1291cba5
Hamilcar Steven Chanjueco <hsb.chanjueco@gmail.com>
2022-08-07 18:02:24 +0800

    added the registration functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   root to: "main#index"
 
   get 'about', to: 'about#index'
+
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
 end
